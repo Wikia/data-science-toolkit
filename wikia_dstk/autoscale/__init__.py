54b91beb5b37fcf58ec7bb887462e64604e64ad6
Tristan Euan Chong <tristaneuan@gmail.com>
2014-02-06 00:18:40 +0000

    initial autoscale commit

---
+++ wikia_dstk/autoscale/__init__.py
@@ -0,0 +1,128 @@
+from boto.ec2 import connect_to_region
+from time import sleep
+
+class EC2Connection(object):
+    """
+    A connection to a specified EC2 region.
+    """
+    def __init__(self, options):
+        """
+        Instantiate a boto.ec2.connection.EC2Connection object upon which to
+        perform actions.
+
+        :type options: dict
+        :param region: A dictionary containing the autoscaling options
+        """
+        self.region = options.get('region')
+        self.price = options.get('price')
+        self.ami = options.get('ami')
+        self.key = options.get('key')
+        self.sec = options.get('sec')
+        if not isinstance(self.sec, list):
+            self.sec = self.sec.split(',')
+        self.type = options.get('type')
+        self.tag = options.get('tag')
+        self.threshold = options.get('threshold')
+        self.max_size = options.get('max_size')
+        self.conn = connect_to_region(self.region)
+
+    def _request_instances(self, count):
+        """
+        Request spot instances.
+
+        :type count: int
+        :param count: The number of spot instances to request
+
+        :rtype: boto.ec2.instance.Reservation
+        :return: The Reservation object representing the spot instance request
+        """
+        return self.conn.request_spot_instances(price=self.price,
+                                                image_id=self.ami,
+                                                count=count,
+                                                key_name=self.key,
+                                                security_groups=self.sec,
+                                                instance_type=self.type)
+
+    def _get_instance_ids(self, reservation):
+        """
+        Get instance IDs for a particular reservation.
+
+        :type reservation: boto.ec2.instance.Reservation
+        :param reservation: A Reservation object created by requesting spot
+                            instances
+
+        :rtype: list
+        :return: A list containing strings representing the instance IDs of the
+                 given Reservation
+        """
+        r_ids = [request.id for request in reservation]
+        while True:
+            sleep(5)
+            requests = self.conn.get_all_spot_instance_requests(request_ids=r_ids)
+            instance_ids = []
+            for request in requests:
+                instance_id = request.instance_id
+                if instance_id is None:
+                    break
+                instance_ids.append(instance_id)
+            if len(instance_ids) < len(reservation):
+                print 'waiting for %d instances to launch...' % len(reservation)
+                continue
+            break
+        return instance_ids
+
+    def _tag_instances(self, instance_ids):
+        """
+        Attach identifying tags to the specified instances.
+
+        :type instance_ids: list
+        :param instance_ids: A list of instance IDs to tag
+        """
+        tags = {'Name': self.tag}
+        self.conn.create_tags(instance_ids, tags)
+
+    def add_instances(self, count):
+        """
+        Add a specified number of instances.
+
+        :type count: int
+        :param count: The number of instances to add
+
+        :rtype: int
+        :return: An integer indicating the number of active tagged instances
+        """
+        # Create spot instances
+        reservation = self._request_instances(count)
+        # Tag created spot instances
+        instance_ids = self._get_instance_ids(reservation)
+        self._tag_instances(instance_ids)
+
+        return len(self.get_tagged_instances())
+
+    def terminate(self, instance_ids):
+        """
+        Terminate instances with the specified IDs.
+
+        :type instance_ids: list
+        :param instance_ids: A list of strings representing the instance IDs to
+                             be terminated
+        """
+        self.conn.terminate_instances(instance_ids)
+
+    def get_tagged_instances(self, tag=None):
+        """
+        Get pending or running instances labeled with the tags specified in the
+        options.
+
+        :type tag: string
+        :param tag: A string representing the tag name to operate over
+
+        :rtype: list
+        :return: A list of strings representing the IDs of the tagged instances
+        """
+        if tag is None:
+            tag = self.tag
+        filters = {'tag:Name': tag}
+        return [instance.id for reservation in
+                self.conn.get_all_instances(filters=filters) for instance in
+                reservation.instances if instance.state_code < 32]
---
+++ wikia_dstk/autoscale/parser_poller.py
@@ -0,0 +1,138 @@
+"""
+This script polls S3 to find new text batches to parse.
+"""
+import os
+import shutil
+import sys
+import tarfile
+from autoscale import EC2Connection
+from boto import connect_s3, connect_ec2
+from boto.s3.key import Key
+from boto.exception import S3ResponseError
+from boto.utils import get_instance_metadata
+from socket import gethostname
+from subprocess import Popen, call
+from time import time, sleep
+from utils import chrono_sort
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+SIG = str(os.getpid()) + '_' + str(int(time()))
+TEXT_DIR = '/tmp/text/'
+XML_DIR = '/tmp/xml/'
+PACKAGE_DIR = "/tmp/event_packages/"
+BUCKET_NAME = 'nlp-data'
+REGION = 'us-west-2'
+
+s3_conn = connect_s3()
+bucket = s3_conn.get_bucket(BUCKET_NAME)
+hostname = gethostname()
+ec2_conn = EC2Connection(dict(region=REGION))
+stalling_increments = 0
+
+def add_files():
+    global hostname, bucket, PACKAGE_DIR, SIG, inqueue
+    print "[%s] Adding to text queue" % hostname
+
+    keys = filter(lambda x:x.key.endswith('.tgz'), bucket.list('text_events/'))
+
+    # iterating over keys in case we try to grab a key that another instance scoops
+    for key in keys:
+        old_key_name = key.key
+        print "[%s] found key %s" % (hostname, old_key_name)
+        # found a tar file, now try to capture it via move
+        try:
+            new_key_name = '/parser_processing/'+SIG+'.tgz'
+            key.copy(bucket, new_key_name)
+            key.delete()
+
+        except S3ResponseError:
+            # we probably hit our race condition -- not to worry!
+            # we'll just take the next key.
+            continue
+
+        # now that it's been moved, pull it down
+        newkey = Key(bucket)
+        newkey.key = new_key_name
+        newfname = PACKAGE_DIR+SIG+'.tgz'
+        newkey.get_contents_to_filename(newfname)
+
+        # untar that sucker
+        print "[%s] Unpacking %s" % (hostname, newfname)
+        tar = tarfile.open(newfname)
+        tar.extractall(TEXT_DIR)
+        tar.close()
+        os.remove(newfname)
+        inqueue = len(os.listdir(TEXT_DIR))
+
+        # delete remnant data with extreme prejudice
+        newkey.delete()
+
+        # at this point we want to get the list of keys all over again
+        return True
+    return False
+
+def is_newest_older_than(duration):
+    """Return True if the most recently modified file in TEXT_DIR is older than
+    a given number of minutes, or if TEXT_DIR is empty"""
+    ordered = chrono_sort(TEXT_DIR)
+    if not ordered:
+        return True
+    newest_file, modified_time = ordered[-1]
+    if (time() - modified_time) / 60 > duration:
+        return True
+    return False
+
+while True:
+    for directory in [TEXT_DIR, XML_DIR, PACKAGE_DIR]:
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+    inqueue = len(os.listdir(TEXT_DIR))
+    
+    if inqueue < 10:
+        added = add_files()
+        # shut this instance down if we have an empty queue and we're above desired capacity
+        if not added and len(os.listdir(XML_DIR)) == 0 and is_newest_older_than(15):
+            instances = ec2_conn.get_tagged_instances('parser')
+            print "[%s] Scaling down, shutting down." % hostname
+            current_id = get_instance_metadata()['instance-id']
+            if len(filter(lambda x: x == current_id, instances)) == 1:
+                ec2_conn.terminate([current_id])
+                sys.exit()
+
+    print "[%s] %d text files in queue..." % (hostname, inqueue)
+
+    data_events = []
+    xmlfiles = os.listdir(XML_DIR)
+
+    if len(xmlfiles) == 0:
+        stalling_increments += 1
+    else:
+        stalling_increments = 0
+
+    if stalling_increments == 5:
+        stalling_increments = 0
+        print "Restarting daemon and adding more files since it's being a slow douche."
+        call("sv stop parser_daemon", shell=True)
+        call("killall java", shell=True)
+        call("sv start parser_daemon", shell=True)
+        print "Done with that. Now get to work!"
+
+    for xmlfile in xmlfiles:
+        key = Key(bucket)
+        new_key = '/xml/%s/%s.xml' % tuple(xmlfile.replace('.xml', '').split('_'))
+        key.key = new_key
+        data_events += [new_key]
+        xmlfilename = XML_DIR+xmlfile
+        key.set_contents_from_filename(xmlfilename)
+        os.remove(xmlfilename)
+
+    print "[%s] Uploaded %d files (rate of %.2f docs/sec)" % (hostname, len(xmlfiles), float(len(xmlfiles))/30.0)
+        
+    # write events to a new file
+    event_key = Key(bucket)
+    event_key.key = '/data_events/'+SIG
+    event_key.set_contents_from_string("\n".join(data_events))
+
+    sleep(30) # don't want to bug the crap outta amazon

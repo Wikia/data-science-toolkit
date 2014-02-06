4d81a16dafdf55c030637f69ceddae4e48b05175
Robert Elwell <robert@wikia-inc.com>
2014-02-05 16:44:03 -0800

    wikia_dstk

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="dstk",
+    name="wikia_dstk",
     version= "0.0.1",
     author="Robert Elwell",
     author_email="robert.elwell@gmail.com",
     description="A library for automated data science scripts that rely on NLP libs, etc.",
     license="Other",
-    packages=["dstk.lda"],
+    packages=["wikia_dstk.lda"],
     install_requires=["nlp_services", "pyro4", "gensim", "scikit-learn", "requests"],
     dependency_links=["https://github.com/relwell/nlp_services/archive/master.zip#egg=nlp_services-0.0.1"],
     )

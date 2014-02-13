069fa804d4542129731e0f38b78be4576d8b5442
Robert Elwell <robert@wikia-inc.com>
2014-02-13 12:34:55 -0800

    use initialized zero matrix

--- wikia_dstk/lda/__init__.py
+++ wikia_dstk/lda/__init__.py
@@ -196,8 +196,12 @@ class WikiaDSTKDictionary(Dictionary):
         wpl_items = word_probabilities_list.items()
         token_ids, probabilities = zip(*wpl_items)
         # padding with zeroes for numpy
-        log("At probabilities")
-        probabilities = np.array([probs + ([0] * (num_documents - len(probs))) for probs in probabilities])
+        log("At probabilities, initializing zero matrix")
+        probs_zeros = np.zeros((len(probabilities), num_documents))
+        for i, probs in enumerate(probabilities):
+            probs_zeros[i][0:len(probs)] = probs
+        probabilities = probs_zeros
+        log("built from zeros")
         dtype = [('token_id', 'i'), ('value', 'f')]
         log("At SAT")
         token_to_sat = zip(token_ids, np.divide(np.mean(probabilities, axis=1), np.var(probabilities, axis=1)))

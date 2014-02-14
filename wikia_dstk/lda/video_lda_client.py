8b1abf3ea37fa1366a8f4fff1a9da73368c3eaca
Robert Elwell <robert@wikia-inc.com>
2014-02-14 14:50:49 -0800

    stupid typo

--- wikia_dstk/lda/video_lda_client.py
+++ wikia_dstk/lda/video_lda_client.py
@@ -159,9 +159,9 @@ chmod 777 /var/log/lda_dispatcher
 echo `date` `hostname -i ` "Starting Nameserver" >> /var/log/my_startup.log
 python -m Pyro4.naming -n 0.0.0.0 > /var/log/name_server  2>&1 &
 echo `date` `hostname -i ` "Starting Dispatcher" >> /var/log/my_startup.log
-python -m gensim.models.lda_dispatcher.py > /var/log/lda_dispatcher 2>&1 &
+python -m gensim.models.lda_dispatcher > /var/log/lda_dispatcher 2>&1 &
 echo `date` `hostname -i ` "Running LDA Server Script" >> /var/log/my_startup.log
-python -m wikia_dstk.lda.video_lda_server.py > /var/log/lda_server 2>&1 &
+python -m wikia_dstk.lda.video_lda_server > /var/log/lda_server 2>&1 &
 echo `date` `hostname -i ` "User Data End" >> /var/log/my_startup.log""" % (args.num_topics, args.max_topic_frequency,
                                                                             args.model_prefix, args.s3_prefix,
                                                                             args.node_count, args.ami, args.master_ip))

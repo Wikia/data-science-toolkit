e8b6e058c54cb824e0eb55677260352c0d5dea6b
Tristan Euan Chong <tristaneuan@gmail.com>
2014-02-15 00:48:03 +0000

    added comma

--- wikia_dstk/pipeline/wiki_data_extraction/config.py
+++ wikia_dstk/pipeline/wiki_data_extraction/config.py
@@ -7,5 +7,15 @@ config = {
              "type": "m2.4xlarge",
              "tag": "wiki_data_extraction",
              "threshold": 50,
-             "max_size": 5
+             "max_size": 5,
+             "services": [
+                 "TopEntitiesService",
+                 "EntityDocumentCountsService",
+                 "TopHeadsService",
+                 "WpTopEntitiesService",
+                 "WpEntityDocumentCountsService",
+                 "WikiEntitySentimentService",
+                 "WpWikiEntitySentimentService",
+                 "AllEntitiesSentimentAndCountsService"
+                         ]
          }

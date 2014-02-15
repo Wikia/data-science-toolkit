69fc67bc772806c07d3d789bd332905db1c99a14
Tristan Euan Chong <tristaneuan@gmail.com>
2014-02-15 01:52:47 +0000

    launch EC2 instances, passing user_data

--- wikia_dstk/pipeline/wiki_data_extraction/launch.py
+++ wikia_dstk/pipeline/wiki_data_extraction/launch.py
@@ -1,10 +1,11 @@
 from __future__ import division
 import requests
 from .. import EC2Connection
+from ..top5k import top5k
+from collections import defaultdict
 from optparse import OptionParser, OptionError
 
 from config import config
-from top5k import top5k
 
 LAST_INDEXED = '/data/last_indexed.txt'
 SOLR = 'http://search-s10:8983/solr/xwiki/select'
@@ -27,6 +28,8 @@ op.add_option('-m', '--max-size', dest='max_size', type='int',
 
 config.update([(k, v) for (k, v) in vars(options).items() if v is not None])
 
+ec2_conn = EC2Connection(config)
+
 # Read the last indexed date
 date = options.date
 if date is None:
@@ -36,9 +39,10 @@ if date is None:
 params = {
              'q':'lang_s:en',
              'fl':'id,articles_i',
-             'rows':'1000', # 10000
+             'rows':'2000', # 10000
+             'sort': 'wam_i desc',
              'facet': 'true',
-             'facet.limit': '100', # -1
+             'facet.limit': '500', # -1
              'facet.query': 'indexed:[%sZ TO NOW]' % date,
              'facet.field': 'id',
              'wt': 'json'
@@ -53,11 +57,24 @@ ids = r['facet_counts']['facet_fields']['id']
 d = dict((ids[i], ids[i+1]) for i in range(0, len(ids), 2))
 
 # Find wikis in top 5k indexed since given date
-wids = filter(lambda x: d.get(x), [l.strip() for l in open('top5k.txt')])
+wids = filter(lambda x: d.get(x), top5k)
 
-# TODO: Split into groups of approx equal total doc count
+# Split into groups of approx equal total doc count, args = {inst #: wids to pass}
 parts = config['max_size']
 wids = sorted(wids, key=lambda x: articles.get(x, 0), reverse=True)
-print wids; import sys; sys.exit(0)
+args = defaultdict(list)
+for i in range(0, len(wids), parts):
+    for n, wid in enumerate(wids[i:i+parts]):
+        args[n].append(wid)
 
-# TODO: Write multiple user_data shell scripts with wids passed as args and launch the appropriate # of instances
+for wid in wids: print wid, articles.get(wid, 0)
+print args
+import sys; sys.exit(0)
+
+# Write individual user_data shell scripts with wids passed as args and launch
+# EC2 instances to execute them one at a time
+for n in args:
+    user_data = """#!/bin/sh
+    /home/ubuntu/venv/bin/python -m wikia_dstk.pipeline.wiki_data_extraction.run %s
+    """ % ','.join(args[n])
+    ec2_conn.add_instances(1, user_data)

1f84ba570339f45c209732d6570cf0a8fdf505f6
Robert Elwell <robert@wikia-inc.com>
2014-02-15 16:45:48 -0800

    update ami

--- wikia_dstk/lda/wiki_lda_client.py
+++ wikia_dstk/lda/wiki_lda_client.py
@@ -7,7 +7,7 @@ from . import run_server_from_args
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--ami', dest='ami', type=str,
-                        default="ami-d6e785e6",
+                        default="ami-6c274a5c",
                         help='The AMI to launch')
     parser.add_argument('--num-nodes', dest='node_count', type=int,
                         default=20,
@@ -37,7 +37,7 @@ def get_args():
                         default=os.getenv('NODE_INSTANCES', 20),
                         help="Number of node instances to launch")
     parser.add_argument('--node-ami', dest='node_ami', type=str,
-                        default=os.getenv('NODE_AMI', "ami-d6e785e6"),
+                        default=os.getenv('NODE_AMI', "ami-6c274a5c"),
                         help="AMI of the node machines")
     parser.add_argument('--dont-terminate-on-complete', dest='terminate_on_complete', action='store_false',
                         default=os.getenv('TERMINATE_ON_COMPLETE', True),

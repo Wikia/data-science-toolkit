5f7ae12440fb4e81aa2a088899dbe140fa80d1d1
Tristan Euan Chong <tristaneuan@gmail.com>
2014-02-18 18:22:26 +0000

    added status print statements

--- wikia_dstk/pipeline/wiki_data_extraction/launch.py
+++ wikia_dstk/pipeline/wiki_data_extraction/launch.py
@@ -36,34 +36,41 @@ if date is None:
     with open(LAST_INDEXED, 'r') as f:
         date = f.read().strip()
 
+print 'Last indexed date:', date
+
 params = {
              'q':'lang_s:en',
              'fl':'id,articles_i',
-             'rows':'2000', # 10000
+             'rows':'10000', # 10000
              'sort': 'wam_i desc',
              'facet': 'true',
-             'facet.limit': '500', # -1
+             'facet.limit': '-1', # -1
              'facet.query': 'indexed:[%sZ TO NOW]' % date,
              'facet.field': 'id',
              'wt': 'json'
          }
 
-# Populate dict - {wid: int indicating whether indexed since given date}
 r = requests.get(SOLR, params=params).json()
+# Populate dict - {wid: number of articles}
 docs = r['response']['docs']
+print 'Populating article count dict...'
 articles = dict((doc['id'], doc.get('articles_i', 0)) for doc in docs)
-#from pprint import pprint; pprint(articles); import sys; sys.exit(0)
+# Populate dict - {wid: int indicating whether indexed since given date}
 ids = r['facet_counts']['facet_fields']['id']
+print 'Populating indexed dict...'
 d = dict((ids[i], ids[i+1]) for i in range(0, len(ids), 2))
 
 # Find wikis in top 5k indexed since given date
+print 'Filtering wiki IDs by indexed since last date...'
 wids = filter(lambda x: d.get(x), top5k)
 
 # Split into groups of approx equal total doc count, args = {inst #: wids to pass}
+print 'Splitting wiki IDs into approximately equal groups...'
 parts = config['max_size']
 wids = sorted(wids, key=lambda x: articles.get(x, 0), reverse=True)
 args = defaultdict(list)
 for i in range(0, len(wids), parts):
+    print '%d/%d complete' % (i, len(wids))
     for n, wid in enumerate(wids[i:i+parts]):
         args[n].append(wid)
 
@@ -73,4 +80,5 @@ for n in args:
     user_data = """#!/bin/sh
     /home/ubuntu/venv/bin/python -m wikia_dstk.pipeline.wiki_data_extraction.run %s
     """ % ','.join(args[n])
+    print 'Launching instance %d of %d' % (n, len(args))
     ec2_conn.add_instances(1, user_data)

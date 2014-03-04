7e20c356fb46de0ae1db467e9823b564c3ffc06b
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-04 20:21:22 +0000

    PEP-8, improve docstrings

--- wikia_dstk/__init__.py
+++ wikia_dstk/__init__.py
@@ -7,40 +7,63 @@ from time import sleep
 
 
 def chrono_sort(directory):
-    """Return a list of files in a directory, sorted chronologically"""
-    files = [(os.path.join(directory, filename), os.path.getmtime(os.path.join(directory, filename))) for filename in os.listdir(directory)]
+    """
+    Return a list of files in a directory, sorted chronologically
+
+    :type directory: string
+    :param directory: A filepath within which to sort files
+
+    :rtype: list
+    :return: A list of tuples having the format (filepath, time of last
+             modification)
+    """
+    files = [(os.path.join(directory, filename),
+              os.path.getmtime(os.path.join(directory, filename))) for filename
+             in os.listdir(directory)]
     files.sort(key=lambda x: x[1])
     return files
 
+
 def ensure_dir_exists(directory):
     """
-    Makes sure the directory given as an argument exists, and returns the same
+    Make sure the directory given as an argument exists, and returns the same
     directory.
+
+    :type directory: string
+    :param directory: A filepath to create if it doesn't already exist
+
+    :rtype: string
+    :return: The filepath originally passed as an argument
     """
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
 
-def run_instances_lb(ids, callable, num_instances, user_data, options=None, ami="ami-2eb7da1e"):
+
+def run_instances_lb(ids, callable, num_instances, user_data, options=None,
+                     ami="ami-dc0c63ec"):
     """
-    Run a set of instances that evenly distributes the workload between target IDs
+    Run a set of instances that evenly distributes the workload between target
+    IDs
 
     :type ids: list
     :param ids: A list of IDs against which to run this process
 
     :type callable: function
-    :param callable: A function that, given an ID, returns a value that should be load-balanced
+    :param callable: A function that, given an ID, returns a value that should
+                     be load-balanced
 
     :type num_instances: int
     :param num_instances: The number of instances to create
 
     :type user_data: string
     :param user_data: The script to run on instantiation, i.e. where the logic
-                      goes. Should contain '%s' to pass comma-separated list of IDs as an
-                      argument via a string-formatting operation
+                      goes. Should contain '%s' to pass comma-separated list of
+                      IDs as an argument via a string-formatting operation
 
     :type options: dict
-    :param options: Launch configuration options with which to instantiate an EC2Connection object
+    :param options: Launch configuration options with which to instantiate an
+                    EC2Connection object
 
     :type ami: string
     :param ami: The AMI ID of the image to load
@@ -53,18 +76,24 @@ def run_instances_lb(ids, callable, num_instances, user_data, options=None, ami=
         options = {'ami': ami}
     conn = EC2Connection(options)
 
-    # TODO: Explore different methods of combinatorial optimization to improve this
+    # TODO: Explore diff methods of combinatorial optimization to improve this
     # Split IDs into buckets of approx equal total 'callable' value
-    ids = sorted(ids, key=lambda x: callable(x), reverse=True) # Sort descending
-    parts = defaultdict(list) # {instance #: list of IDs to pass to instance}
+    ids = sorted(ids, key=lambda x: callable(x), reverse=True)  # Sort desc
+    parts = defaultdict(list)  # {instance #: list of IDs to pass to instance}
     for i in range(0, len(ids), num_instances):
         for n, id_ in enumerate(ids[i:i+num_instances]):
             parts[n].append(id_)
 
-    #return str([sum([callable(i) for i in parts[n]]) for n in parts]) # Bin sizes
+    # Bin size
+    #return str([sum([callable(i) for i in parts[n]]) for n in parts])
 
-    # Format user_data script with comma-separated list of IDs, and launch instances
-    return conn.add_instances_async(num_instances, [user_data % ','.join([str(id_) for id_ in ids]) for ids in parts.values()])
+    # Format user_data script with comma-separated list of IDs, and launch
+    # instances
+    scripts = [user_data % ','.join([str(wid) for wid in wids]) for wids in
+               parts.values()]
+    return scripts
+    return conn.add_instances_async(num_instances, scripts)
+
 
 class EC2Connection(object):
     """A connection to a specified EC2 region."""
@@ -104,19 +133,17 @@ class EC2Connection(object):
         :return: A list of IDs corresponding to the instances launched
         """
         # Request spot instances
-        reservation = self.conn.request_spot_instances(price=self.price,
-                                                       image_id=self.ami,
-                                                       count=count,
-                                                       key_name=self.key,
-                                                       security_groups=self.sec,
-                                                       user_data=user_data,
-                                                       instance_type=self.type)
+        reservation = self.conn.request_spot_instances(
+            price=self.price, image_id=self.ami, count=count,
+            key_name=self.key, security_groups=self.sec, user_data=user_data,
+            instance_type=self.type)
 
         # Get instance IDs for the reservation
         r_ids = [request.id for request in reservation]
-        while True: # Because the requests are fulfilled independently
+        while True:  # Because the requests are fulfilled independently
             sleep(5)
-            requests = self.conn.get_all_spot_instance_requests(request_ids=r_ids)
+            requests = self.conn.get_all_spot_instance_requests(
+                request_ids=r_ids)
             instance_ids = []
             for request in requests:
                 instance_id = request.instance_id
@@ -124,7 +151,7 @@ class EC2Connection(object):
                     break
                 instance_ids.append(instance_id)
             if len(instance_ids) < len(reservation):
-                print 'Waiting for %d instances to launch...' % len(reservation)
+                print 'Waiting for %d instances to launch' % len(reservation)
                 continue
             break
 
@@ -136,7 +163,8 @@ class EC2Connection(object):
 
     def add_instances_async(self, count, user_data_scripts):
         """
-        Add a specified number of instances asynchronously, each with unique user_data.
+        Add a specified number of instances asynchronously, each with unique
+        user_data.
 
         :type count: int
         :param count: The number of instances to add

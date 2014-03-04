1e2a6a1024b761d722d62c5ae6374b33668db5a3
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-04 17:50:16 +0000

    update AMI ID to base-wdx-140304

--- wikia_dstk/pipeline/wiki_data_extraction/config.py
+++ wikia_dstk/pipeline/wiki_data_extraction/config.py
@@ -1,7 +1,7 @@
 config = {
              "region": "us-west-2",
              "price": "0.300",
-             "ami": "ami-08244b38",  # base-wdx-140303f
+             "ami": "ami-040d6234",  # base-wdx-140304
              "key": "data-extraction",
              "sec": "sshable",
              "type": "m2.4xlarge",

ce8b0f6629c44af76c94f8e6f3325d5a72937d7f
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-05 00:30:40 +0000

    PEP-8

--- wikia_dstk/pipeline/event/write.py
+++ wikia_dstk/pipeline/event/write.py
@@ -5,15 +5,23 @@ from datetime import datetime, timedelta
 # split into multiple event files to facilitate multiprocessing
 
 LAST_INDEXED = '/data/last_indexed.txt'
-SPLIT = 4 # Number of event files to split the time delta into
+SPLIT = 4  # Number of event files to split the time delta into
+
 
 def total_seconds(td):
     """
     Return the total number of seconds in a datetime.timedelta object
-
     Explicitly defined, since official implementation is missing from
-    datetime.timedelta before Python 2.7"""
-    return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+    datetime.timedelta before Python 2.7
+
+    :type td: datetime.timedelta
+    :param td: A timedelta object for which to calculate # of seconds
+
+    :rtype: float
+    :return: The number of seconds in the timedelta object
+    """
+    return ((td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) /
+            10**6)
 
 # Read the date this script was last run
 with open(LAST_INDEXED, 'r') as f:
@@ -25,7 +33,8 @@ now = datetime.utcnow()
 # Split the time delta into equal parts
 delta = total_seconds(now - last_indexed)
 increment = delta / SPLIT
-delimiters = [(last_indexed + timedelta(seconds=increment*i)) for i in range(SPLIT)] + [now]
+delimiters = [(last_indexed + timedelta(seconds=increment*i)) for i in
+              range(SPLIT)] + [now]
 
 # Write the current date to file for future use
 with open(LAST_INDEXED, 'w') as f:
@@ -33,6 +42,8 @@ with open(LAST_INDEXED, 'w') as f:
 
 # Write multiple Solr queries to the events directory
 for i in range(SPLIT):
-    query = 'iscontent:true AND lang:en AND wam:[50 TO *] AND indexed:[%sZ TO %sZ]' % (datetime.isoformat(delimiters[i]), datetime.isoformat(delimiters[i+1]))
+    from_ = datetime.isoformat(delimiters[i])
+    to = datetime.isoformat(delimiters[i+1])
+    query = 'iscontent:true AND lang:en AND indexed:[%sZ TO %sZ]' % (from_, to)
     with open('/data/events/%d' % i, 'w') as f:
         f.write(query)

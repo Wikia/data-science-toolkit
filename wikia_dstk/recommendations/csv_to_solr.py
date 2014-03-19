9c2db5b31a8101cdee668a85081dab62881fabf5
Robert Elwell <robert@wikia-inc.com>
2014-03-19 11:39:37 -0700

    bug fixes

--- wikia_dstk/recommendations/csv_to_solr.py
+++ wikia_dstk/recommendations/csv_to_solr.py
@@ -8,7 +8,7 @@ import os
 
 def get_args():
     ap = ArgumentParser()
-    ap.add_argument('--solr-url', dest='solr_url', default='http://dev-search:8983/', help="The Solr endpoint")
+    ap.add_argument('--solr-url', dest='solr_url', default='http://dev-search:8983/solr/', help="The Solr endpoint")
     ap.add_argument('--solr-core', dest='solr_core', default='main', help="Which core in Solr to use")
     ap.add_argument('--infile', dest='infile', type=FileType('r'), help="A file on the local machine")
     ap.add_argument('--s3path', dest='s3path', help="All CSVs in a given folder, or a single CSV")
@@ -23,15 +23,15 @@ def get_s3_files(s3path):
     bucket = connect_s3().get_bucket('nlp-data')
     keys = bucket.get_all_keys(prefix=s3path)
     for key in keys:
-        filename = key.split('/')[-1]
+        filename = key.name.split('/')[-1]
         key.get_contents_to_filename(filename)
         yield open(filename, 'r')
 
 
 def send_solr_update(args, docs):
     solr_url = "%s/%s/update/" % (args.solr_url, args.solr_core)
-    resp = requests.post(solr_url, data=json.dumps(docs))
-    if resp.status != 200:
+    resp = requests.post(solr_url, data=json.dumps(docs), headers={'Content-type': 'application/json'})
+    if resp.status_code != 200:
         raise Exception(resp.content)
 
 
@@ -50,7 +50,7 @@ def main():
         batch = []
         for line in fl:
             split = line.split(',')
-            batch.append({'id': split[0], args.recommendations_field: split[1:]})
+            batch.append({'id': split[0], args.recommendations_field: {'set': split[1:]}})
             if len(batch) == args.batch_size:
                 send_solr_update(args, batch)
                 batch = []
@@ -59,3 +59,6 @@ def main():
         fl.close()
         if args.s3path:
             os.remove(fl.name)
+
+if __name__ == '__main__':
+    main()

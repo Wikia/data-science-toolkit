4b55bba08c3492ea7b103be1a29c76dacdaf7c0e
Robert Elwell <robert@wikia-inc.com>
2014-03-21 09:18:36 -0700

    add back defaults for get_argparser_from_config

--- wikia_dstk/__init__.py
+++ wikia_dstk/__init__.py
@@ -43,7 +43,8 @@ def get_argparser_from_config(default_config):
     """
     ap = ArgumentParser()
     for key in default_config:
-        ap.add_argument('--%s' % key, type=type(default_config[key]), dest=key.replace('-', '_'))
+        ap.add_argument('--%s' % key, type=type(default_config[key]), dest=key.replace('-', '_'),
+                        default=default_config[key])
     ap.set_defaults(**default_config)
     return ap
 

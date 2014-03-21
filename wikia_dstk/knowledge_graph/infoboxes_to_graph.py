04b3e43c315adc45af028ef1a1dd2038cc203425
Robert Elwell <robert@wikia-inc.com>
2014-03-21 16:28:21 -0700

    fixed a bunch of shit

--- wikia_dstk/knowledge_graph/infoboxes_to_graph.py
+++ wikia_dstk/knowledge_graph/infoboxes_to_graph.py
@@ -23,45 +23,58 @@ def escape_key(string):
 def handle_doc(tup):
     args, doc = tup
     db = GraphDatabase(args.graph_db)
-    name = doc[u'title_en'].replace(u'"', u'')
+    name = doc[u'title_en'].replace(u'"', u'').lower()
     print name.encode(u'utf8')
-    name_index = db.nodes.indexes.get('name')
-    name_nodes = [node for node in name_index[name]]
+    name_index = db.nodes.indexes.get(u'name')
+    wiki_index = db.nodes.indexes.get(u'wiki_ids')
+    name_nodes = [node for node in name_index[u'name'][name]]
     page_ids = [doc[u'id']]
     if not name_nodes:
         page_node = db.nodes.create(ids=page_ids, name=name)
         page_node.labels.add(u'Page')
+        name_index[u'name'][name] = page_node
     else:
         page_node = name_nodes[0]
-        #page_node[u'ids'] = page_node[u'ids'] + page_ids
+        page_node[u'ids'] = page_node[u'ids'] + page_ids
 
     box_nodes = []
     for line in doc[u'infoboxes_txt']:
         splt = line.split(u'|')
         if len(splt) > 2:
             key = splt[1].lower().strip(u':')
-            print "\t", key
             value = u'|'.join(splt[2:]).lower()
-            prop = [node for node in name_index.get(value)]
-            if not prop:
+            props = [node for node in name_index['name'][value]]
+            if not props:
                 box_node = db.nodes.create(name=value)
-                box_node.labels.add(u'Object')
+                if u"Object" not in box_node.labels:
+                    box_node.labels.add(u'Object')
+                name_index['name'][value] = box_node
             else:
-                box_node = prop[0]
-            print box_node
-            db.relationships.create(box_node, 'is_%s_of' % key, page_node)
-            page_node.labels.add(u'Subject')
+                box_node = props[0]
+            try:
+                db.relationships.create(box_node, u'is_%s_of' % key, page_node)
+            except Exception as e:
+                print e
+            if u"Subject" not in page_node.labels:
+                page_node.labels.add(u'Subject')
             box_nodes.append(box_node)
 
-    wiki_nodes = [node for node in db.nodes.indexes.get('wiki_ids')[doc[u'wid']]]
+    wiki_nodes = [node for node in wiki_index[u'wiki_id'][doc[u'wid']]]
     if not wiki_nodes:
         wiki_node = db.nodes.create(wiki_id=doc[u'wid'])
         wiki_node.labels.add(u'Wiki')
+        wiki_index[u'wiki_id'][doc[u'wid']] = wiki_node
     else:
         wiki_node = wiki_nodes[0]
-    db.relationships.create(wiki_node, u'involves', page_node)
+    try:
+        db.relationships.create(wiki_node, u'involves', page_node)
+    except Exception as e:
+        print e
     for infobox_node in box_nodes:
-        db.relationships.create(wiki_node, u'involves', infobox_node)
+        try:
+            db.relationships.create(wiki_node, u'involves', infobox_node)
+        except Exception as e:
+            print e
 
 
 def run_queries(args, pool, start=0):
@@ -70,20 +83,21 @@ def run_queries(args, pool, start=0):
     response = requests.get(u'%s/select' % args.solr, params=query_params).json()
 
     pool.map(handle_doc, [(args, doc) for doc in response[u'response'][u'docs']])
-    if response['response']['numFound'] > query_params['start']:
-        return run_queries(args, pool, start+query_params['rows'])
+    if response[u'response'][u'numFound'] > query_params[u'start']:
+        return run_queries(args, pool, start+query_params[u'rows'])
     return True
 
 
 def main():
     args = get_args()
     db = GraphDatabase(args.graph_db)
-    db.nodes.indexes.create('wiki_ids')
-    db.nodes.indexes.create('name')
+    for label in [u'Page', u'Object', u'Subject', u'Wiki']:
+        try:
+            db.labels.create(label)
+        except:
+            continue
     pool = Pool(processes=args.num_processes)
     run_queries(args, pool)
-
-
 
 
 if __name__ == '__main__':

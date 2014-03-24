0aab300f9f5754a989fbbfba50e2adf09572b2af
Robert Elwell <robert@wikia-inc.com>
2014-03-24 12:16:35 -0700

    try again

--- wikia_dstk/knowledge_graph/download_parses.py
+++ wikia_dstk/knowledge_graph/download_parses.py
@@ -1,5 +1,6 @@
 from boto import connect_s3
 from argparse import ArgumentParser
+from os import makedirs
 
 
 def get_args():
@@ -12,8 +13,9 @@ def get_args():
 def main():
     args = get_args()
     bucket = connect_s3().get_bucket("nlp-data")
+    makedirs("%s/%s" % (args.dest, args.wiki_id))
     for key in bucket.list(prefix='xml/%s' % args.wiki_id):
-        fname = "%s/%s/%s" % (args.dest, args.wiki_id, key.name)
+        fname = "%s/%s/%s" % (args.dest, args.wiki_id, key.name.split('/')[-1])
         print key, '->', fname
         key.get_contents_to_filename(fname)
 

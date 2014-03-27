6b32dc874dd63ba0a0c7f5ab66cf693607b5c45c
Robert Elwell <robert@wikia-inc.com>
2014-03-27 13:50:30 -0700

    utf8bs

--- wikia_dstk/authority/create_database.py
+++ wikia_dstk/authority/create_database.py
@@ -12,13 +12,13 @@ import MySQLdb as mdb
 
 def get_args():
     ap = ArgumentParser()
-    ap.add_argument('--host', dest='host', default='localhost')
-    ap.add_argument('-u', '--user', dest='user', default='root')
-    ap.add_argument('-p', '--password', dest='password', default='root')
-    ap.add_argument('-d', '--database', dest='database', default='authority')
-    ap.add_argument('-s', '--s3path', dest='s3path', default='datafiles/topwams.txt')
-    ap.add_argument('-w', '--no-wipe', dest='wipe', default=True, action='store_false')
-    ap.add_argument('-n', '--num-processes', dest='num_processes', default=6)
+    ap.add_argument(u'--host', dest=u'host', default=u'localhost')
+    ap.add_argument(u'-u', u'--user', dest=u'user', default=u'root')
+    ap.add_argument(u'-p', u'--password', dest=u'password', default=u'root')
+    ap.add_argument(u'-d', u'--database', dest=u'database', default=u'authority')
+    ap.add_argument(u'-s', u'--s3path', dest=u's3path', default=u'datafiles/topwams.txt')
+    ap.add_argument(u'-w', u'--no-wipe', dest=u'wipe', default=True, action=u'store_false')
+    ap.add_argument(u'-n', u'--num-processes', dest=u'num_processes', default=6)
     return ap.parse_known_args()
 
 
@@ -26,16 +26,17 @@ def create_tables(args):
     db = get_db_connection(args)
     cursor = db.cursor()
 
-    print "Creating tables"
+    print u"Creating tables"
 
     if args.wipe:
-        cursor.execute("DROP DATABASE IF EXISTS authority")
+        cursor.execute(u"DROP DATABASE IF EXISTS authority")
 
-    cursor.execute("CREATE DATABASE IF NOT EXISTS authority")
-    cursor.execute("USE authority")
+    cursor.execute(u"""CREATE DATABASE IF NOT EXISTS authority 
+                            DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;""")
+    cursor.execute(u"USE authority")
 
-    print "\tCreating table wikis..."
-    cursor.execute("""
+    print u"\tCreating table wikis..."
+    cursor.execute(u"""
     CREATE TABLE  wikis (
       wiki_id INT PRIMARY KEY NOT NULL,
       wam_score FLOAT NULL,
@@ -43,10 +44,10 @@ def create_tables(args):
       url VARCHAR(255) NULL,
       authority FLOAT NULL
     ) ENGINE=InnoDB
-    """)
+    u""")
 
-    print "\tCreating table articles..."
-    cursor.execute("""
+    print u"\tCreating table articles..."
+    cursor.execute(u"""
     CREATE TABLE  articles (
       doc_id varchar(255) PRIMARY KEY NOT NULL,
       article_id INT NOT NULL,
@@ -57,39 +58,39 @@ def create_tables(args):
       FOREIGN KEY (wiki_id) REFERENCES wikis(wiki_id),
       UNIQUE KEY (article_id, wiki_id)
     ) ENGINE=InnoDB
-    """)
+    u""")
 
-    print "\tCreating table users..."
-    cursor.execute("""
+    print u"\tCreating table users..."
+    cursor.execute(u"""
     CREATE TABLE  users (
       user_id INT PRIMARY KEY NOT NULL,
       username varchar(255) NOT NULL,
       total_authority FLOAT NULL
     ) ENGINE=InnoDB
-    """)
+    u""")
 
-    print "\tCreating table topics..."
-    cursor.execute("""
+    print u"\tCreating table topics..."
+    cursor.execute(u"""
     CREATE TABLE  topics (
       topic_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
       name VARCHAR(255) NOT NULL,
       total_authority FLOAT NULL,
       UNIQUE KEY (name)
     ) ENGINE=InnoDB
-    """)
+    u""")
 
-    print "\tCreating table articles_users..."
-    cursor.execute("""
+    print u"\tCreating table articles_users..."
+    cursor.execute(u"""
     CREATE TABLE  articles_users (
       doc_id VARCHAR(255) NOT NULL,
       user_id INT NOT NULL,
       authority FLOAT NOT NULL,
       UNIQUE KEY (doc_id, user_id)
     ) ENGINE=InnoDB
-    """)
+    u""")
 
-    print "\tCreating table topics_users..."
-    cursor.execute("""
+    print u"\tCreating table topics_users..."
+    cursor.execute(u"""
     CREATE TABLE topics_users (
       topic_id INT NOT NULL,
       user_id INT NOT NULL,
@@ -98,10 +99,10 @@ def create_tables(args):
       FOREIGN KEY (user_id) REFERENCES users(user_id),
       UNIQUE KEY (topic_id, user_id)
     ) ENGINE= InnoDB
-    """)
+    u""")
 
-    print "\tCreating table articles_topics..."
-    cursor.execute("""
+    print u"\tCreating table articles_topics..."
+    cursor.execute(u"""
     CREATE TABLE articles_topics (
       topic_id INT NOT NULL,
       article_id INT NOT NULL,
@@ -112,40 +113,41 @@ def create_tables(args):
       FOREIGN KEY (wiki_id) REFERENCES wikis(wiki_id),
       UNIQUE KEY (topic_id, wiki_id, article_id)
     ) ENGINE= InnoDB
-    """)
+    u""")
 
-    print "Created all tables"
+    print u"Created all tables"
 
 
 def insert_data(args):
     use_caching(is_read_only=True, shouldnt_compute=True)
     db = get_db_connection(args)
     cursor = db.cursor()
-    cursor.execute("USE authority")
+    cursor.execute(u"USE authority")
 
-    print "Inserting wiki data for", args.wid
+    print u"Inserting wiki data for", args.wid
 
-    items = requests.get('http://www.wikia.com/api/v1/Wikis/Details', params={'ids': args.wid}).json().get('items')
+    items = requests.get(u'http://www.wikia.com/api/v1/Wikis/Details', params={u'ids': args.wid}).json().get(u'items')
     if not items:
         return False
 
     wiki_data = items[args.wid]
 
-    cursor.execute("""
-    INSERT INTO wikis (wiki_id, wam_score, title, url) VALUES (%s, %s, "%s", "%s")
-    """ % (args.wid, str(wiki_data['wam_score']), wiki_data['title'], wiki_data['url']))
+    cursor.execute(u"""
+    INSERT INTO wikis (wiki_id, wam_score, title, url) VALUES (%s, %s, u"%s", u"%s")
+    u""" % (args.wid, str(wiki_data[u'wam_score']).encode(u'utf8'),
+            wiki_data[u'title'].encode(u'utf8'), wiki_data[u'url'].encode(u'utf8')))
 
     authority_dict = WikiAuthorityService().get_value(args.wid)
     if not authority_dict:
         return False
 
-    print "Inserting authority data for pages on wiki", args.wid
+    print u"Inserting authority data for pages on wiki", args.wid
     for key in authority_dict:
-        splt = key.split('_')
+        splt = key.split(u'_')
         wiki_id, article_id = splt[-2], splt[-1]   # fix stupid bug
-        cursor.execute("""
-        INSERT INTO articles (doc_id, article_id, wiki_id, local_authority) VALUES ("%s", %s, %s, %s)
-        """ % (key, article_id, wiki_id, str(authority_dict[key])))
+        cursor.execute(u"""
+        INSERT INTO articles (doc_id, article_id, wiki_id, local_authority) VALUES (u"%s", %s, %s, %s)
+        u""" % (key, article_id, wiki_id, str(authority_dict[key])))
 
 
 def get_db_connection(args):
@@ -157,20 +159,20 @@ def main():
 
     start = time.time()
     create_tables(args)
-    bucket = connect_s3().get_bucket('nlp-data')
-    print "Getting and filtering wiki IDs"
-    if os.path.exists('cached_wids'):
-        wids = [line.strip() for line in open('cached_wids', 'r').readlines() if line.strip()]
+    bucket = connect_s3().get_bucket(u'nlp-data')
+    print u"Getting and filtering wiki IDs"
+    if os.path.exists(u'cached_wids'):
+        wids = [line.strip() for line in open(u'cached_wids', u'r').readlines() if line.strip()]
     else:
         wids = filter_wids([line.strip()
-                            for line in bucket.get_key(args.s3path).get_contents_as_string().split("\n")
+                            for line in bucket.get_key(args.s3path).get_contents_as_string().split(u"\n")
                             if line.strip()], True)
-        open('cached_wids', 'w').write("\n".join(wids))
+        open(u'cached_wids', u'w').write(u"\n".join(wids))
     p = Pool(processes=args.num_processes)
-    print "Inserting data"
+    print u"Inserting data"
     p.map_async(insert_data, [Namespace(wid=wid, **vars(args)) for wid in wids]).get()
-    print "Finished in", (time.time() - start), "seconds"
+    print u"Finished in", (time.time() - start), u"seconds"
 
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     main()

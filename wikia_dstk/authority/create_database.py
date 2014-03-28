459e7a0c55cd9343cc525ca27088b3ba587af20a
Robert Elwell <robert@wikia-inc.com>
2014-03-28 08:55:57 -0700

    steps in a row

--- wikia_dstk/authority/create_database.py
+++ wikia_dstk/authority/create_database.py
@@ -6,7 +6,6 @@ from nlp_services.caching import use_caching
 from nlp_services.authority import WikiAuthorityService, PageAuthorityService
 from nlp_services.discourse.entities import WikiPageToEntitiesService
 import os
-import json
 import traceback
 import time
 import requests
@@ -129,106 +128,157 @@ def my_escape(s):
     return s.replace(u'\\', u'').replace(u'"', u'').replace(u"'", u'')
 
 
-def insert_data(args):
-    try:
-        use_caching(is_read_only=True, shouldnt_compute=True)
-        db = get_db_connection(args)
-        cursor = db.cursor()
-        cursor.execute(U'USE authority')
-
-        print u"Inserting wiki data for", args.wid
-
-        response = requests.get(u'http://www.wikia.com/api/v1/Wikis/Details',
-                                params={u'ids': args.wid})
-
-        items = response.json().get(u'items')
-        if not items:
-            return False
-
-        wiki_id = args.wid
-        wiki_data = items[args.wid]
-
-        cursor.execute(u"""
-        INSERT INTO wikis (wiki_id, wam_score, title, url) VALUES (%s, %s, "%s", "%s")
-        """ % (args.wid, str(wiki_data[u'wam_score']),
-               db.escape(wiki_data[u'title']), wiki_data[u'url']))
-        db.commit()
-
-        authority_dict = WikiAuthorityService().get_value(args.wid)
-        if not authority_dict:
-            return False
-
-        print u"Inserting authority data for pages on wiki", args.wid
-        authority_dict_fixed = dict([(key.split(u'_')[-2]+u'_'+key.split(u'_')[-1], val)
-                                     for key, val in authority_dict.items()])
-
-        wpe = WikiPageToEntitiesService().get_value(wiki_id)
-        if not wpe:
-            print u"NO WIKI PAGE TO ENTITIES SERVICE FOR", wiki_id
-            return False
-
-        print u"Priming entity data on", wiki_id
-        for page, entity_data in wpe.items():
-            entity_list = map(my_escape,
-                              list(set(entity_data.get(u'redirects', {}).values() + entity_data.get(u'titles'))))
-            for i in range(0, len(entity_list), 20):
-                cursor.execute(u"""
-                INSERT IGNORE INTO topics (name) VALUES ("%s")
-                """ % u'"), ("'.join(entity_list[i:i+20]))
-                db.commit()
-
-        print u"Inserting page and author and contrib data for wiki", wiki_id
-        for doc_id in authority_dict_fixed:
-            wiki_id, article_id = doc_id.split(u'_')
-            cursor.execute(u"""
-            INSERT INTO articles (doc_id, article_id, wiki_id, local_authority) VALUES ("%s", %s, %s, %s)
-            """ % (doc_id, article_id, wiki_id, str(authority_dict_fixed[doc_id])))
-
-            entity_data = wpe.get(article_id, {})
-            entity_list = map(my_escape,
-                              list(set(entity_data.get(u'redirects', {}).values() + entity_data.get(u'titles', []))))
-            cursor.execute(u"""
-            SELECT topic_id FROM topics WHERE name IN ("%s")
-            """ % (u'", "'.join(entity_list)))
-            topic_ids = []
-            for result in cursor.fetchall():
-                topic_ids.append(result[0])
-                cursor.execute(u"""
-                INSERT INTO articles_topics (article_id, wiki_id, topic_id) VALUES (%s, %s, %s)
-                """ % (article_id, wiki_id, result[0]))
-                db.commit()
-
-            cursor = db.cursor()
-
-            for contribs in PageAuthorityService().get_value(doc_id, []):
-                cursor.execute(u"""
-                INSERT IGNORE INTO users (user_id, user_name) VALUES (%d, "%s")
-                """ % (contribs[u'userid'], my_escape(contribs[u'user'])))
-                db.commit()
-
-                cursor.execute(u"""
-                INSERT IGNORE INTO articles_users (article_id, wiki_id, user_id, contribs) VALUES (%s, %s, %d, %s)
-                """ % (article_id, wiki_id, contribs[u'userid'], contribs[u'contribs']))
-                db.commit()
-
-                local_authority = contribs[u'contribs'] * authority_dict_fixed.get(page, 0)
-                for topic_id in topic_ids:
-                    cursor.execute(u"""
-                    INSERT INTO topics_users (user_id, topic_id, local_authority) VALUES (%d, %s, %s)
-                    ON DUPLICATE KEY UPDATE local_authority = local_authority + %s
-                    """ % (contribs[u'userid'], topic_id, local_authority, local_authority))
-                db.commit()
-        db.commit()
-        print u"Done with", args.wid
-
-    except Exception as e:
-        print e, traceback.format_exc()
-        raise e
+def wrapped(func):
+    def wrapper(*args, **kwargs):
+        try:
+            use_caching(is_read_only=True, shouldnt_compute=True)
+            func(*args, **kwargs)
+        except Exception as e:
+            print e, traceback.format_exc()
+            raise e
+    return wrapper
 
 
 def get_db_connection(args):
     return mdb.connect(host=args.host, user=args.user, passwd=args.password,
                        use_unicode=True, charset=u'utf8')
+
+
+@wrapped
+def insert_entities(args):
+    db,  cursor = get_db_and_cursor(args)
+
+    wpe = WikiPageToEntitiesService().get_value(args.wid)
+    if not wpe:
+        print u"NO WIKI PAGE TO ENTITIES SERVICE FOR", args.wid
+        return False
+
+    print u"Priming entity data on", args.wid
+    for page, entity_data in wpe.items():
+        entity_list = map(my_escape,
+                          list(set(entity_data.get(u'redirects', {}).values() + entity_data.get(u'titles'))))
+        for i in range(0, len(entity_list), 50):
+            cursor.execute(u"""
+            INSERT IGNORE INTO topics (name) VALUES ("%s")
+            """ % u'"), ("'.join(entity_list[i:i+50]))
+            db.commit()
+    return args.wid
+
+
+@wrapped
+def insert_pages(args):
+    db,  cursor = get_db_and_cursor(args)
+
+    authority_dict_fixed = get_authority_dict_fixed(args)
+    if not authority_dict_fixed:
+        return False
+
+    print u"Inserting authority data for pages on wiki", args.wid
+
+    args = []
+    for doc_id in authority_dict_fixed:
+            wiki_id, article_id = doc_id.split(u'_')
+            args.append((doc_id, article_id, wiki_id, str(authority_dict_fixed[doc_id])))
+
+    cursor.execute(u"""
+        INSERT INTO articles (doc_id, article_id, wiki_id, local_authority) VALUES %s
+        """ % u", ".join([u"""("%s", %s, %s, %s)""" % arg for arg in args]))
+
+    db.commit()
+    return args.wid
+
+
+@wrapped
+def insert_wiki_ids(args):
+    db,  cursor = get_db_and_cursor(args)
+
+    print u"Inserting wiki data for", args.wid
+
+    response = requests.get(u'http://www.wikia.com/api/v1/Wikis/Details',
+                            params={u'ids': args.wid})
+
+    items = response.json().get(u'items')
+    if not items:
+        return False
+
+    wiki_data = items[args.wid]
+
+    cursor.execute(u"""
+    INSERT INTO wikis (wiki_id, wam_score, title, url) VALUES (%s, %s, "%s", "%s")
+    """ % (args.wid, str(wiki_data[u'wam_score']),
+           db.escape(wiki_data[u'title']), wiki_data[u'url']))
+    db.commit()
+    return args.wid
+
+
+@wrapped
+def insert_contrib_data(args):
+    db,  cursor = get_db_and_cursor(args)
+    wpe = WikiPageToEntitiesService().get_value(args.wid)
+    if not wpe:
+        print u"NO WIKI PAGE TO ENTITIES SERVICE FOR", args.wid
+        return False
+    authority_dict_fixed = get_authority_dict_fixed(args)
+    if not authority_dict_fixed:
+        return False
+    print u"Inserting page and author and contrib data for wiki", args.wid
+    for doc_id in authority_dict_fixed:
+        wiki_id, article_id = doc_id.split(u'_')
+
+        entity_data = wpe.get(article_id, {})
+        entity_list = map(my_escape,
+                          list(set(entity_data.get(u'redirects', {}).values() + entity_data.get(u'titles', []))))
+
+        cursor.execute(u"""
+        SELECT topic_id FROM topics WHERE name IN ("%s")
+        """ % (u'", "'.join(entity_list)))
+        topic_ids = []
+        for result in cursor.fetchall():
+            topic_ids.append(result[0])
+            cursor.execute(u"""
+            INSERT INTO articles_topics (article_id, wiki_id, topic_id) VALUES (%s, %s, %s)
+            """ % (article_id, wiki_id, result[0]))
+            db.commit()
+
+        cursor = db.cursor()
+
+        for contribs in PageAuthorityService().get_value(doc_id, []):
+            cursor.execute(u"""
+            INSERT IGNORE INTO users (user_id, user_name) VALUES (%d, "%s")
+            """ % (contribs[u'userid'], my_escape(contribs[u'user'])))
+            db.commit()
+
+            cursor.execute(u"""
+            INSERT IGNORE INTO articles_users (article_id, wiki_id, user_id, contribs) VALUES (%s, %s, %d, %s)
+            """ % (article_id, wiki_id, contribs[u'userid'], contribs[u'contribs']))
+            db.commit()
+
+            local_authority = contribs[u'contribs'] * authority_dict_fixed.get(doc_id, 0)
+            for topic_id in topic_ids:
+                cursor.execute(u"""
+                INSERT INTO topics_users (user_id, topic_id, local_authority) VALUES (%d, %s, %s)
+                ON DUPLICATE KEY UPDATE local_authority = local_authority + %s
+                """ % (contribs[u'userid'], topic_id, local_authority, local_authority))
+            db.commit()
+    db.commit()
+    print u"Done with", args.wid
+    return args.wid
+
+
+def get_authority_dict_fixed(args):
+    authority_dict = WikiAuthorityService().get_value(args.wid)
+    if not authority_dict:
+        return False
+
+    return dict([(key.split(u'_')[-2]+u'_'+key.split(u'_')[-1], val)
+                 for key, val in authority_dict.items()])
+
+
+def get_db_and_cursor(args):
+    db = get_db_connection(args)
+    cursor = db.cursor()
+    cursor.execute(U'USE authority')
+    return db, cursor
 
 
 def main():
@@ -247,7 +297,12 @@ def main():
         open(u'cached_wids', u'w').write(u"\n".join(wids))
     p = Pool(processes=args.num_processes)
     print u"Inserting data"
-    p.map_async(insert_data, [Namespace(wid=wid, **vars(args)) for wid in wids]).get()
+    pipeline = [insert_wiki_ids, insert_pages, insert_entities, insert_contrib_data]
+    wiki_args = [Namespace(wid=wid, **vars(args)) for wid in wids]
+    for step in pipeline:
+        wiki_args = filter(lambda x: x, p.map_async(step, wiki_args).get())
+
+    print len(wiki_args), u"/", len(wids), u"wikis made it through the pipeline"
     print u"Finished in", (time.time() - start), u"seconds"
 
 

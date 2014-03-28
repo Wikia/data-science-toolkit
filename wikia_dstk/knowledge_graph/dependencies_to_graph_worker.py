7d9dd0fefd73304944721b55e677765222bb936e
Robert Elwell <robert@wikia-inc.com>
2014-03-28 16:27:37 -0700

    stupid nodes

--- wikia_dstk/knowledge_graph/dependencies_to_graph_worker.py
+++ wikia_dstk/knowledge_graph/dependencies_to_graph_worker.py
@@ -59,8 +59,7 @@ def main():
         document = etree.fromstring(r.content)
         wiki_id = args.base_uri.split(u'/')[-2]
         doc_id = args.base_uri.split(u'/')[-1].split(u'.')[0]
-        for wrapper in document:
-            print etree.tostring(wrapper)
+        for wrapper in document[0]:
             sentence = wrapper.get(u'sentence')
             for dependency in wrapper[0]:
                 try:

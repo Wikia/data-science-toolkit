06151a33d29c567054995ac4f0b7f6022d8baae5
Robert Elwell <robert@wikia-inc.com>
2014-03-28 14:45:27 -0700

    support multiple wikis

--- wikia_dstk/knowledge_graph/dependencies_to_graph.py
+++ wikia_dstk/knowledge_graph/dependencies_to_graph.py
@@ -2,41 +2,64 @@ import requests
 from lxml import etree
 from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient.request import NotFoundError
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from multiprocessing import Pool
 
 
 def get_args():
     ap = ArgumentParser()
     ap.add_argument(u'-d', u'--exist-db', dest=u'exist_db', default=u'http://nlp-s3:8080')
     ap.add_argument(u'-j', u'--neo4j', dest=u'neo4j', default=u'http://nlp-s3:7474')
+    ap.add_argument(u'-w', u'--wiki-id', dest=u'wiki_id', required=True)
+    ap.add_argument(u'-n', u'--num-processes', dest=u'num_processes', default=6, type=int)
     return ap.parse_args()
 
 
-def get_query(offset=1, limit=500):
+def get_query(wid, offset=1, limit=500):
     return u"""<query xmlns="http://exist.sourceforge.net/NS/exist" start="%d" max="%d"><text>
 xquery version "3.0";
-let $documents := collection("/db/nlp/")
+let $documents := collection("/db/%s/")
 for $document in $documents
     for $dependencies in $document//dependencies[@type='collapsed-ccprocessed-dependencies']
         for $dependency in $dependencies
             return &lt;dependencywrapper base-uri="{fn:base-uri($document)}" sentence="{$dependency/../@id}"&gt;
                     {$dependency}
                    &lt;/dependencywrapper&gt;
-</text></query>""" % (offset, limit)
+</text></query>""" % (wid, offset, limit)
 
 
-def node_from_index(db, index, doc, sentence, word_xml):
-    doc_sent_id = doc + u'_' + sentence
-    word_id = word_xml.get('idx')
+def node_from_index(db, wiki_id, doc, sentence, word_xml):
+    sentence_index = db.nodes.indexes.get(u'sentence')
+    wiki_word_index = db.nodes.indexes.get(u'wiki_word')
+    doc_sent_id = u"_".join([wiki_id, doc, sentence])
+    word_id = word_xml.get(u'idx')
     word = word_xml.text.encode(u'utf8')
-    word_nodes = [node for node in index[doc_sent_id][word_id]]
+    word_nodes = [node for node in sentence_index[doc_sent_id][word_id]]
     if not word_nodes:
-        word_node = db.nodes.create(word=word, doc=doc, sentence=sentence, word_id=word_id)
+        word_node = db.nodes.create(word=word, doc=doc, sentence=sentence, word_id=word_id, wiki_id=wiki_id)
         word_node.labels.add(u'Word')
-        index[doc_sent_id][word_id] = word_node
+        sentence_index[doc_sent_id][word_id] = word_node
+        words = wiki_word_index[wiki_id][word]
+        if words:
+            wiki_word_index[wiki_id][word] += [word_node]
+        else:
+            wiki_word_index[wiki_id][word] = [word_node]
     else:
         word_node = word_nodes[0]
     return word_node
+
+
+def process_dependency(args):
+    db = GraphDatabase(args.neo4j)
+    doc = args.dependencies_wrapper.get(u'base-uri').split(u'/')[-1].split(u'.')[0]
+    sentence = args.dependencies_wrapper.get(u'sentence')
+    for dependency in args.dependencies_wrapper[0]:
+        try:
+            governor = node_from_index(db, args.wiki_id, doc, sentence, dependency[0])
+            dependent = node_from_index(db, args.wiki_id, doc, sentence, dependency[1])
+            db.relationships.create(governor, dependency.get(u'type'), dependent)
+        except IndexError:
+            continue
 
 
 def main():
@@ -51,27 +74,21 @@ def main():
         pass
 
     try:
-        sentence_index = db.nodes.indexes.get(u'sentence')
+        db.nodes.indexes.get(u'sentence')
     except NotFoundError:
-        sentence_index = db.nodes.indexes.create(u'sentence')
+        db.nodes.indexes.create(u'sentence')
+
+    p = Pool(processes=args.num_processes)
+
+    p.map_async(process_dependency)
 
     while True:
         r = requests.post(u'%s/exist/rest/db/' % args.exist_db,
-                          data=get_query(offset, limit),
+                          data=get_query(args.wiki_id, offset, limit),
                           headers={u'Content-type': u'application/xml'})
         dom = etree.fromstring(r.content)
-        for dependencieswrapper in dom:
-            doc = dependencieswrapper.get(u'base-uri').split(u'/')[-1].split(u'.')[0]
-            sentence = dependencieswrapper.get(u'sentence')
-            for dependency in dependencieswrapper[0]:
-                try:
-                    governor = node_from_index(db, sentence_index, doc, sentence, dependency[0])
-                    dependent = node_from_index(db, sentence_index, doc, sentence, dependency[1])
-                    db.relationships.create(governor, dependency.get(u'type'), dependent)
-                except IndexError:
-                    continue
-
-        hits = dom.get('{http://exist.sourceforge.net/NS/exist}hits')
+        p.map_async(process_dependency, [Namespace(dependencies_wrapper=d, **args) for d in dom]).get()
+        hits = dom.get(u'{http://exist.sourceforge.net/NS/exist}hits')
         if not hits:
             print r.content
             break
@@ -81,5 +98,5 @@ def main():
         offset += limit
 
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     main()

f52992b7a27123dbce1afa9b827adb28178e7e19
Tristan Euan Chong <tristaneuan@gmail.com>
2014-04-01 17:07:21 +0000

    update parser AMI to parser-140401

--- wikia_dstk/pipeline/parser/config.py
+++ wikia_dstk/pipeline/parser/config.py
@@ -2,7 +2,7 @@ config = {
     "queue": "text_events",
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-403e5e70",
+    "ami": "ami-4efb937e",  # parser-140401
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",

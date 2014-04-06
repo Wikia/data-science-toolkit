9610eb01ffb431ef320cf0af2585685c3bd1c405
Robert Elwell <robert@wikia-inc.com>
2014-04-06 14:54:31 -0700

    args and stuff

--- wikia_dstk/authority/scale_authority_globally.py
+++ wikia_dstk/authority/scale_authority_globally.py
@@ -22,7 +22,7 @@ def scale_authority_pv(args):
                    SET local_authority_pv = local_authority
                                           * (((pageviews - %0.5f)/(1-%0.5f)) + %0.5f)
                    WHERE wiki_id = %d"""
-               % (min_pv, (max_pv - min_pv), + args.smoothing), args.wiki_id)
+               % (min_pv, (max_pv - min_pv), + args.smoothing, args.wiki_id))
         cursor.execute(sql)
         db.commit()
 

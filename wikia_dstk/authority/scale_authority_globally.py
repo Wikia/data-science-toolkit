65a945e4aa514f8879242ce6ee2b4b46ef08a4b7
Robert Elwell <robert@wikia-inc.com>
2014-04-06 14:44:58 -0700

    quote bug

--- wikia_dstk/authority/scale_authority_globally.py
+++ wikia_dstk/authority/scale_authority_globally.py
@@ -17,11 +17,10 @@ def scale_authority_pv(args):
     wam = cursor.fetchone()[0]
     cursor.execute(u"SELECT MAX(pageviews), MIN(pageviews) FROM articles WHERE wiki_id = %d" % args.wiki_id)
     max_pv, min_pv = cursor.fetchone()
-    sql = (u""""UPDATE articles
-                SET local_authority_pv = local_authority
-                                       * ((pageviews - %0.5f)/(1-%0.5f)) + %0.5f)"""
+    sql = (u"""UPDATE articles
+               SET local_authority_pv = local_authority
+                                      * ((pageviews - %0.5f)/(1-%0.5f)) + %0.5f)"""
            % (min_pv, (max_pv - min_pv), + args.smoothing))
-    print sql
     cursor.execute(sql)
     db.commit()
 

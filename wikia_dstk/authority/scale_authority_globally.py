4252dd3a5783c7404b572ea0c2e9f98e790f24a4
Robert Elwell <robert@wikia-inc.com>
2014-04-07 09:12:38 -0700

    fix minmax scaling bug

--- wikia_dstk/authority/scale_authority_globally.py
+++ wikia_dstk/authority/scale_authority_globally.py
@@ -23,7 +23,7 @@ def scale_authority_pv(args):
             return
         sql = (u"""UPDATE articles
                    SET local_authority_pv = local_authority
-                                          * (((pageviews - %0.5f)/(1-%0.5f)) + %0.5f)
+                                          * (((pageviews - %0.5f)/%0.5f) + %0.5f)
                    WHERE wiki_id = %d"""
                % (min_pv, (max_pv - min_pv), + args.smoothing, args.wiki_id))
         cursor.execute(sql)

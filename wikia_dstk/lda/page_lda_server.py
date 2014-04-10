c5af11f1436463855967fc528c5fc19c50dde9e0
Tristan Chong <tristanchong@gmail.com>
2014-04-10 22:23:27 +0000

    temp commit, contains code from test_indexer

--- wikia_dstk/lda/page_lda_server.py
+++ wikia_dstk/lda/page_lda_server.py
@@ -13,8 +13,12 @@ from nlp_services.title_confirmation import preprocess
 from nlp_services.discourse.entities import WikiPageToEntitiesService
 from boto import connect_s3
 from datetime import datetime
+from multiprocessing import Pool
 from . import normalize, launch_lda_nodes, terminate_lda_nodes, harakiri
 from . import log, get_dct_and_bow_from_features, write_csv_and_text_data
+from ..pipeline.event import QueryIterator
+
+STEP = 50
 
 
 def get_args():
@@ -61,6 +65,78 @@ def get_args():
                     default=os.getenv('GIT_REF', 'master'),
                     help="A DSTK repo ref (tag, branch, commit hash) to check out")
     return ap.parse_args()
+
+
+def chunks(array, n):
+    """Yield successive n-sized chunks from array"""
+    for i in xrange(0, len(array), n):
+        yield array[i:i+n]
+
+
+def get_fields(doc_ids):
+    print 'Getting fields for %s' % doc_ids
+    array = []
+    r = requests.get(
+        '%swikia.php' % url,
+        params={'controller': 'WikiaSearchIndexer',
+                'method': 'get',
+                'service': 'All',
+                #'ids': doc_ids_subset}
+                'ids': '|'.join(doc_ids)}
+        )
+    try:
+        indexer = r.json().get('contents', [])
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except:
+        print traceback.format_exc()
+        indexer = []
+    #pprint(indexer)
+    if indexer:
+        for doc in indexer:
+            if doc.get('id') is not None:
+                array.append(
+                    (doc['id'],
+                     (doc.get('headings_mv_%s' % lang, {}).get('set', []) +
+                      doc.get('categories_mv_%s' % lang, {}).get('set', []))))
+                #indexed[doc['id']] = (
+                #    doc.get('headings_mv_%s' % lang, {}).get('set', []) +
+                #    doc.get('categories_mv_%s' % lang, {}).get('set', []))
+    return array
+
+#foo = range(100)
+#print map(lambda x:x, chunks(foo, 9))
+#sys.exit(0)
+
+details = requests.get(
+    'http://www.wikia.com/api/v1/Wikis/Details/',
+    params={'ids': wid}).json().get('items', {}).get(wid)
+#doc_ids_combined = {}
+#indexed = {}
+if details is not None:
+    url = details.get('url')
+    lang = details.get('lang')
+    #print url
+    #doc_ids = ListDocIdsService().get_value(wid)
+    doc_ids = map(lambda x: x.split('_')[1],
+                  filter(lambda y: '_' in y,
+                         #ListDocIdsService().get_value(wid)))[:100]
+                         ListDocIdsService().get_value(wid)))
+    #pprint(doc_ids); sys.exit(0)
+    #for n in range(0, len(doc_ids), step):
+    ##for n in range(0, 20, step):
+    #    print 'n = %d' % n
+    #    doc_ids_subset = doc_ids[n:n+step]
+    r = Pool(processes=8).map_async(get_fields, chunks(doc_ids, step))
+    r.wait()
+    pprint(r.get())
+    print '*'*80
+    #for k in r.get():  # DEBUG
+    #    print k
+    fields = []
+    m = map(lambda x: fields.extend(x), r.get())
+    #pprint(fields)
+    indexed = dict(fields)
 
 
 def get_data(wid):

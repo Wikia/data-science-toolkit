e5ef20ea1f6807756fae356bb8b36846996b91d6
Tristan Chong <tristanchong@gmail.com>
2014-04-21 21:45:23 +0000

    copied etl from video_lda_client, needs modification

--- wikia_dstk/lda/page_lda_client.py
+++ wikia_dstk/lda/page_lda_client.py
@@ -58,6 +58,56 @@ def get_args():
     return parser.parse_args()
 
 
+def doc_to_vectors(doc):
+    try:
+        data = [d.encode('utf-8') for d in
+                [doc[u'id']]
+                + unis_bis(doc[u'title_en'].replace(u'File:', u''))
+                + map(normalize, doc.get(u'video_actors_txt', []))
+                + map(normalize, doc.get(u'video_tags_txt', []))
+                + map(normalize, doc.get(u'categories_mv_en', []))
+                + map(normalize, doc.get(u'video_tags_txt', []))
+                + map(normalize, doc.get(u'video_genres_txt', []))
+                + [ubt for li in doc.get(u'video_description_txt', []) for ubt in unis_bis(li)]
+                + [ubt for li in doc.get(u'html_media_extras_txt', []) for ubt in unis_bis(li)]
+                ]
+        return dict([(data[0], data[1:])])
+    except (IndexError, TypeError) as e:
+        log(e)
+        return []
+
+
+def etl_concurrent(pool):
+    """
+    Asynchronously handle ETL to reduce wait time due to HTTP request blocking
+    """
+    log("Extracting data...")
+    params = {'wt': 'json', 'rows': 0, 'fl': '*', 'q': 'wid:298117 AND is_video:true'}
+    response = requests.get('http://search-s10:8983/solr/main/select', params=params).json()
+    log(response['response']['numFound'], "videos")
+    r = pool.map_async(get_docs, range(0, response['response']['numFound'], 500))
+    r.wait()
+    docs = [doc for docset in r.get() for doc in docset]
+    log("Got all docs, now building features")
+    doclen = len(docs)
+    features = {}
+    for i in range(0, doclen, 5000):
+        log("%.2f%%" % (float(i)/float(doclen) * 100))
+        map(features.update, pool.map_async(doc_to_vectors, docs[i:i+5000]).get())
+    return features
+
+
+def get_docs(start):
+    """
+    Functional core of ETL
+    """
+    log(start)
+    return requests.get('http://search-s10:8983/solr/main/select',
+                        params={'wt': 'json',
+                        'start': start,
+                        'rows': 500,
+                        'fl': '*',
+                        'q': 'wid:298117 AND is_video:true'}).json().get('response', {}).get('docs', [])
 def main():
     args = get_args()
     run_server_from_args(

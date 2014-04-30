92d936b9d20f1f0f1ff27efdddb38ee2ee98a212
Tristan Chong <tristanchong@gmail.com>
2014-04-30 20:13:51 +0000

    use multiprocessing.Pool

--- wikia_dstk/pipeline/wiki_data_extraction/child.py
+++ wikia_dstk/pipeline/wiki_data_extraction/child.py
@@ -1,7 +1,6 @@
 import os
 import sys
-import traceback
-from boto import connect_s3
+from multiprocessing import Pool
 
 from nlp_services.caching import use_caching
 from config import config
@@ -17,7 +16,8 @@ from nlp_services.authority import *
 
 def get_args():
     ap = get_argparser_from_config(config)
-    ap.add_argument('-w', '--wiki-id', dest='wiki_id', required=True, help="The wiki ID to operate over")
+    ap.add_argument('-w', '--wiki-id', dest='wiki_id', required=True,
+                    help="The wiki ID to operate over")
     return ap.parse_known_args()
 
 
@@ -31,18 +31,20 @@ def main():
     use_caching(per_service_cache=caching_dict)
 
     print 'Calling wiki-level services on %s' % args.wiki_id
-    try:
-        for service in services:
-            try:
-                print args.wiki_id, service
-                getattr(sys.modules[__name__], service)().get(args.wiki_id)
-                caching_dict[service+'.get'] = {'dont_compute': True}  # DRY!
-                use_caching(per_service_cache=caching_dict)
-            except Exception as e:
-                print args.wiki_id, service, e
-    except Exception as e:
-        print args.wiki_id, e
+
+    def get_service(service):
+        print args.wiki_id, service
+        try:
+            getattr(sys.modules[__name__], service)().get(args.wiki_id)
+        except Exception as e:
+            print e
+
+    pool = Pool(processes=8)
+    s = pool.map_async(services, get_service)
+    s.wait()
 
 
 if __name__ == '__main__':
+    caching_dict[service+'.get'] = {'dont_compute': True}  # DRY!
+    use_caching(per_service_cache=caching_dict)
     main()

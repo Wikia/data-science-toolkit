157ba1223ce533acb3747bcd1961a95593d6b67d
Tristan Chong <tristanchong@gmail.com>
2014-05-08 19:02:05 +0000

    change to tristan's branch of nlp_services temporarily

--- wikia_dstk/pipeline/wiki_data_extraction/launch.py
+++ wikia_dstk/pipeline/wiki_data_extraction/launch.py
@@ -75,7 +75,7 @@ def main():
     callback = lambda x: articles.get(x, 0)
     num_instances = config['max_size']
     user_data = """#!/bin/sh
-sudo pip install -e git+https://github.com/relwell/nlp_services#egg=nlp_services
+sudo pip install -e git+https://github.com/tristaneuan/nlp_services#egg=nlp_services
 cd /home/ubuntu/data-science-toolkit
 git fetch origin
 git checkout {git_ref}

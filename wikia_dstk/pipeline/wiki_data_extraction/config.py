adb0f117cd45921eb69ea4ce63735994b4966a1a
Tristan Chong <tristanchong@gmail.com>
2014-05-08 23:26:56 +0000

    change ami to dstk tristan

--- wikia_dstk/pipeline/wiki_data_extraction/config.py
+++ wikia_dstk/pipeline/wiki_data_extraction/config.py
@@ -1,7 +1,7 @@
 config = {
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-13156323",  # dstk v3.4
+    "ami": "ami-611e6851",  # dstk tristan
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",

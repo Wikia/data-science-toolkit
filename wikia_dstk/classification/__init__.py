9f996ac598e1d6ae4e1772ba68fe728b886a44f5
Robert Elwell <robert@wikia-inc.com>
2014-05-12 14:08:01 -0700

    librarying out

---
+++ wikia_dstk/classification/__init__.py
@@ -0,0 +1,74 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.lda import LDA
+from sklearn.qda import QDA
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+vertical_labels = dict(
+    comics=[2233, 631, 330278, 2446, 405961, 47003, 566447, 198946, 4385, 2237],
+    tv=[130814, 18733, 1228, 26337, 1581, 971, 4095, 831, 684, 13346, 3200],
+    games=[304, 3125, 300851, 208733, 490, 14764, 3035, 3510, 462, 3646, 147],
+    music=[263695, 43339, 84, 22001, 710614, 2006, 79512, 4446, 113969, 203914],
+    lifestyle=[11557, 283, 425, 40, 78127, 322043, 3638, 4486, 544419, 3355, 8390],
+    books=[1575, 7045, 935, 114341, 15738, 694030, 12244, 265480, 12331, 379, 509, 35171, 147, 159],
+    movies=[509, 35171, 147, 6294, 559, 177996, 9231, 1668, 159, 277726, 613758, 6954]
+)
+
+
+class Classifiers():
+    classifiers = {
+        u"Nearest Neighbors": (KNeighborsClassifier, [3], dict()),
+        u"Linear SVM": (SVC, [], dict(kernel="linear", C=0.025)),
+        u"RBF_SVM": (SVC, [], dict(gamma=2, C=1)),
+        u"Decision Tree": (DecisionTreeClassifier, [], dict(max_depth=999)),
+        u"Random_Forest": (RandomForestClassifier, [], dict(max_depth=999, n_estimators=100, max_features=2)),
+        u"AdaBoost": (AdaBoostClassifier, [], {}),
+        u"Naive Bayes": (GaussianNB, [], {}),
+        u"LDA": (LDA, [], {}),
+        u"QDA": (QDA, [], {}),
+        u"Maximum Entropy": (LogisticRegression, [], {}),
+    }
+
+    classifier_keys_to_names = {
+        u"knn": u"Nearest Neighbors",
+        u"linear_svm": u"Linear SVM",
+        u"rbf_svm": u"RBF_SVM",
+        u"decision_tree": u"Decision Tree",
+        u"random_forest": u"Random_Forest",
+        u"adaboost": u"AdaBoost",
+        u"naive_bayes": u"Naive Bayes",
+        u"lda": u"LDA",
+        u"qda": u"QDA",
+        u"maxent": u"Maximum Entropy",
+    }
+
+    @classmethod
+    def get(cls, val):
+        """
+        Powers lazy instantiation of a class
+        """
+        if val not in cls.classifier_keys_to_names:
+            raise KeyError(u"Classifier not configured for %s" % val)
+
+        return apply(apply, cls.classifier_keys_to_names[val])
+
+    def __getattr__(self, item):
+        return self.get(item)
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def each(cls):
+        for key in cls.classifier_keys_to_names:
+            yield cls.get(key)
+
+    @classmethod
+    def each_with_name(cls):
+        for key in cls.classifier_keys_to_names:
+            yield cls.classifier_keys_to_names[key], cls.get(key)
--- wikia_dstk/classification/test_classifiers.py
+++ wikia_dstk/classification/test_classifiers.py
@@ -1,15 +1,8 @@
 import sys
 import traceback
 import time
+from . import vertical_labels, Classifiers
 from collections import OrderedDict
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.lda import LDA
-from sklearn.qda import QDA
-from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from multiprocessing import Pool
 
@@ -17,16 +10,7 @@ from multiprocessing import Pool
 def main():
     fl = open(sys.argv[1], u'r')
 
-    groups = dict(
-        comics=[2233, 631, 330278, 2446, 405961, 47003, 566447, 198946, 4385, 2237],
-        tv=[130814, 18733, 1228, 26337, 1581, 971, 4095, 831, 684, 13346, 3200],
-        games=[304, 3125, 300851, 208733, 490, 14764, 3035, 3510, 462, 3646, 147],
-        music=[263695, 43339, 84, 22001, 710614, 2006, 79512, 4446, 113969, 203914],
-        lifestyle=[11557, 283, 425, 40, 78127, 322043, 3638, 4486, 544419, 3355, 8390],
-        books=[1575, 7045, 935, 114341, 15738, 694030, 12244, 265480, 12331, 379, 509, 35171, 147, 159],
-        movies=[509, 35171, 147, 6294, 559, 177996, 9231, 1668, 159, 277726, 613758, 6954]
-    )
-
+    groups = vertical_labels
     print u"Loading CSV..."
     wid_to_features = OrderedDict([(splt[0], u" ".join(splt[1:])) for splt in
                                    [line.decode(u'utf8').strip().split(u',') for line in fl]
@@ -59,23 +43,10 @@ def main():
              )
         )
 
-    classifiers = {
-        #u"Nearest Neighbors": KNeighborsClassifier(3),
-        #u"Linear SVM": SVC(kernel="linear", C=0.025),
-        u"RBF_SVM": SVC(gamma=2, C=1),
-        u"Decision Tree": DecisionTreeClassifier(max_depth=999),
-        u"Random_Forest": RandomForestClassifier(max_depth=999, n_estimators=100, max_features=2),
-        #u"AdaBoost": AdaBoostClassifier(),
-        u"Naive Bayes": GaussianNB(),
-        #u"LDA": LDA(),
-        #u"QDA": QDA(),
-        u"Maximum Entropy": LogisticRegression(),
-    }
-
     print u"Running leave-one-out cross-validation..."
 
     p = Pool(processes=8)
-    print p.map_async(classify, [(i, loo_args) for i in classifiers.items()]).get()
+    print p.map_async(classify, [(i, loo_args) for i in Classifiers.each()]).get()
 
 
 def classify(arg_tup):

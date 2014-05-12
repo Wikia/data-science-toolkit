8fbfb9909ce8863baf630fa58dec2685dfef140d
Tristan Chong <tristanchong@gmail.com>
2014-05-12 17:21:05 +0000

    increase max size to 5

--- wikia_dstk/pipeline/wiki_data_extraction/config.py
+++ wikia_dstk/pipeline/wiki_data_extraction/config.py
@@ -8,7 +8,7 @@ config = {
     "tag": "wiki_data_extraction",
     "threshold": 50,
     "git_ref": "page-rec",  # master
-    "max_size": 1,  # 5
+    "max_size": 5,  # 5
     "services": ",".join([
         "TopEntitiesService",
         "EntityDocumentCountsService",

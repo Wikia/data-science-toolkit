c5848a681709dc9d94532e3491c7e796152a524a
Tristan Chong <tristanchong@gmail.com>
2014-05-19 17:27:43 +0000

    update AMI and git-ref to dstk v3.4 and master

--- wikia_dstk/pipeline/wiki_data_extraction/config.py
+++ wikia_dstk/pipeline/wiki_data_extraction/config.py
@@ -1,13 +1,13 @@
 config = {
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-611e6851",  # dstk tristan
+    "ami": "ami-13156323",  # dstk v3.4
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",
     "tag": "wiki_data_extraction",
     "threshold": 50,
-    "git_ref": "page-rec",  # master
+    "git_ref": "master",
     "max_size": 5,  # 5
     "services": ",".join([
         "TopEntitiesService",

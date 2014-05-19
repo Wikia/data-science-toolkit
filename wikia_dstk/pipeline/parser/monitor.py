3a60db45bba3ce739c222ee895a7eb8510c81cc8
Tristan Chong <tristanchong@gmail.com>
2014-05-19 23:11:28 +0000

    add 2>&1

--- wikia_dstk/pipeline/parser/monitor.py
+++ wikia_dstk/pipeline/parser/monitor.py
@@ -27,7 +27,7 @@ echo `date` `hostname -i ` "Updating DSTK" >> /var/log/my_startup.log
 git fetch origin
 git checkout %s
 git pull origin %s && sudo python setup.py install
-sudo python -m wikia_dstk.pipeline.parser.run | tee -a /var/log/parser.log
+sudo python -m wikia_dstk.pipeline.parser.run 2>&1 | tee -a /var/log/parser.log
 """ % (args.git_ref, args.git_ref)
 
 s3_conn = connect_s3()

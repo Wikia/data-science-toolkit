5ed206720faa3e56741cf7e7702c3e5cdc418d02
Tristan Chong <tristanchong@gmail.com>
2014-05-20 23:16:48 +0000

    reminder to change AMI

--- wikia_dstk/pipeline/parser/config.py
+++ wikia_dstk/pipeline/parser/config.py
@@ -2,7 +2,7 @@ default_config = {
     "queue": "text_events",
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-5f97e66f",  # parser-140519
+    "ami": "ami-5f97e66f",  # parser-140519 TODO: this will be changed once AMI is created
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",

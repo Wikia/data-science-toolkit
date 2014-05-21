56fb2969a60d611178e56dbfd94a1c918bc58178
Tristan Chong <tristanchong@gmail.com>
2014-05-21 01:03:09 +0000

    update AMI to parser-140520b w/ new aws credentials

--- wikia_dstk/pipeline/parser/config.py
+++ wikia_dstk/pipeline/parser/config.py
@@ -2,7 +2,7 @@ default_config = {
     "queue": "text_events",
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-9de190ad",  # parser-140520
+    "ami": "ami-6fe0915f",  # parser-140520b
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",

4916331b572ce5e2dc84de792da2fd9e8331b439
Robert Elwell <robert@wikia-inc.com>
2014-05-27 14:19:36 -0700

    yay new http

--- wikia_dstk/pipeline/exist/__init__.py
+++ wikia_dstk/pipeline/exist/__init__.py
@@ -14,7 +14,7 @@ def xml_to_exist(args, xml, wiki_id, page_id):
     :type page_id: str
     """
 
-    con = httplib.HTTP(args.url.replace('http://'))
+    con = httplib.HTTP(args.url.replace('http://', ''))
     con.putrequest('PUT', '/exist/nlp/%s/%s' % (wiki_id, page_id))
     con.putheader('Content-Type', 'application/xml')
     con.putheader('Content-Length', '%d' % len(xml))

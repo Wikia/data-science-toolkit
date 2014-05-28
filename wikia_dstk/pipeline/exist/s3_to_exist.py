700446a1a1b1493b4b173990be9a79f6b2f847f9
Robert Elwell <robert@wikia-inc.com>
2014-05-28 10:11:47 -0700

    without concurrency now

--- wikia_dstk/pipeline/exist/s3_to_exist.py
+++ wikia_dstk/pipeline/exist/s3_to_exist.py
@@ -35,7 +35,7 @@ def for_wid(args, wid):
     print u"Working on", wid
     bucket = connect_s3().get_bucket(u'nlp-data')
     pool = Pool(processes=args.threads)
-    pool.imap(key_to_exist, bucket.list(prefix=u'xml/%s/' % wid), 100)
+    map(key_to_exist, bucket.list(prefix=u'xml/%s/' % wid), 100)
 
 
 def main():

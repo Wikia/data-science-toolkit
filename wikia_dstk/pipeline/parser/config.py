aafd3938f03a0ca47ae4726779450d9bb0872b07
Robert Elwell <robert@wikia-inc.com>
2014-06-09 16:42:04 -0700

    updated ami for improved serviceness

--- wikia_dstk/pipeline/parser/config.py
+++ wikia_dstk/pipeline/parser/config.py
@@ -2,7 +2,7 @@ default_config = {
     "queue": "text_events",
     "region": "us-west-2",
     "price": "0.300",
-    "ami": "ami-6fe0915f",  # parser-140520b
+    "ami": "ami-3584f605",  # parser-140520c
     "key": "data-extraction",
     "sec": "sshable",
     "type": "m2.4xlarge",
